413ca6ead3e7831f6faba33d725aafcd762b95de
Alex Wong <alex@10xgenomics.com>
2017-07-04 21:46:37 -0700

    Fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-DIR=$(dirname "$0")
-
-cd $DIR/..
-
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -17,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -27,3 +23,6 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Pushing to origin"
+git push origin gh-pages
